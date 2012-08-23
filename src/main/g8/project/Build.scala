043f6bc52c69bde6136f3512b2e775246663f9e0
Björn Antonsson <ban@rhsh.net>
2012-08-23 12:28:32 +0200

    Updated akka=2.1-M2, scala=2.10.0-M7

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -11,12 +11,16 @@ object ProjectBuild extends Build {
       organization := "$organization$",
       version := "$version$",
       scalaVersion := "$scala_version$",
-      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+      // FIXME: "scalaBinaryVersion" is only needed for Scala Milestones
+      scalaBinaryVersion in Global := "$scala_version$",
+      resolvers ++= Seq("Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+        "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"),
       libraryDependencies ++= Seq(
-        "com.typesafe.akka" % "akka-actor"      % "$akka_version$",
-        "com.typesafe.akka" % "akka-testkit"    % "$akka_version$"  % "test",
-        "junit"             % "junit"           % "4.10"            % "test",
-        "org.scalatest"     % "scalatest_2.10.0-M6" % "1.9-2.10.0-M6-B2" % "test")
+        "com.typesafe.akka" %% "akka-actor"      % "$akka_version$",
+        "com.typesafe.akka" %% "akka-testkit"    % "$akka_version$"   % "test",
+        "junit"             % "junit"            % "4.10"             % "test",
+        // FIXME: convert to %% when scalatest is published correctly
+        "org.scalatest"     % "scalatest"        % "1.9-2.10.0-M7-B1" % "test" cross CrossVersion.full)
     )
   )
 }
